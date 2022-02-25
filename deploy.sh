c0b867479a334f48aa223ef5a075af7b210122b5
swiresupport <99775510+swiresupport@users.noreply.github.com>
2022-02-24 19:19:30 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,6 @@
 #Application for Class10 deployment
+this is a great project
+hopefully it will be followed by a Readme file
+readme file is often the first file which users read. 
+it could be a text file that contains key info for users about the software, project, code, & game etc 
+sometmes, it might contain instructions, help, or details about patches or updates.
